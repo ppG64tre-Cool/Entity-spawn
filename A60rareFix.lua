47182ef138b50b7d2efb2691fccd85bc2e0dfb94
ppG64tre-Cool <pptaratorn@gmail.com>
2026-08-28 09:11:50 +0700

    Fix asset URL format in A60rareFix.lua

--- A60rareFix.lua
+++ A60rareFix.lua
@@ -12,7 +12,7 @@ local lighting = game.Lighting
 local entity = spawner.Create({
 	Entity = {
 		Name = "A60",
-		Asset = "https://github.com/ppG64tre-Cool/Entity-spawn/blob/main/ARE%20YOU%20SERI3.rbxmx?raw=true",
+		Asset = "https://github.com/ppG64tre-Cool/Entity-spawn/raw/main/ARE YOU SERI3.rbxmx",
 		HeightOffset = 1
 	},
 	Lights = {
