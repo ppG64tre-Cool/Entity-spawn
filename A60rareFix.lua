6444ed1a9c6025cfd7d6dfce4a94cdfc5be1c237
ppG64tre-Cool <pptaratorn@gmail.com>
2026-08-28 10:51:34 +0700

    Fix primaryPart CFrame assignment

--- A60rareFix.lua
+++ A60rareFix.lua
@@ -207,7 +207,7 @@ entity:SetCallback("OnDamagePlayer", function(newHealth)
 
 			-- Bám theo camera
 			local desiredPos = camera.CFrame.Position + camera.CFrame.LookVector * 4
-			primaryPart.CFrame = CFrame.new(desiredPos.Position, camera.CFrame.Position).Position
+			primaryPart.CFrame = CFrame.new(desiredPos.Position)
 			camera.CFrame = CFrame.lookAt(camera.CFrame.Position, primaryPart.Position)
 		end)
 
