54219f11b5027bdf750334a9e101acb5dc29949a
ppG64tre-Cool <pptaratorn@gmail.com>
2026-09-11 20:13:31 +0700

    Remove sound cleanup for played sound effect
    Removed sound cleanup from Debris after playing.

--- A60rareFix.lua
+++ A60rareFix.lua
@@ -190,7 +190,6 @@ entity:SetCallback("OnDamagePlayer", function(newHealth)
 		sound.SoundId = "rbxassetid://132942725846535"
 		sound.Volume = 10
 		sound:Play()
-	    Debris:AddItem(sound,20)
 
 		-- A-60 đến sát người chơi
 		primaryPart.Anchored = true
