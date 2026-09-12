633697772b253036af0f66763eb0d4229a7b1b51
ppG64tre-Cool <pptaratorn@gmail.com>
2026-09-12 20:20:37 +0700

    Update spawner URL in A60newDesing.lua

--- A60newDesing.lua
+++ A60newDesing.lua
@@ -1,6 +1,6 @@
 -- Import spawner
 
-local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/DOORS-Entity-Spawner-V2/main/init.luau"))()
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
 local CameraShaker = require(game.ReplicatedStorage.CameraShaker)
 local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
