f191ef61110fd661fe51e9a4df768cc96df4ff8d
ppG64tre-Cool <pptaratorn@gmail.com>
2026-09-17 11:56:59 +0700

    Update A60rareFix.lua

--- A60rareFix.lua
+++ A60rareFix.lua
@@ -1,6 +1,6 @@
 -- Import spawner
 
-local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+local spawner = loadstring(game:HttpGet("https://github.com/ppG64tre-Cool/ppg64tre-modifire/raw/main/initSpawner.luau"))()
 local CameraShaker = require(game.ReplicatedStorage.CameraShaker)
 local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
