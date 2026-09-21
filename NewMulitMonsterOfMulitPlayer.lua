124db251f0ae4aa5b3f79d69fe91656b450aafaf
ppG64tre-Cool <pptaratorn@gmail.com>
2026-09-21 21:34:00 +0700

    Update NewMulitMonsterOfMulitPlayer.lua

--- NewMulitMonsterOfMulitPlayer.lua
+++ NewMulitMonsterOfMulitPlayer.lua
@@ -214,7 +214,7 @@ local function SummonMulitMonster()
 			-- Bám theo camera
 			local desiredPos = camera.CFrame.Position + camera.CFrame.LookVector * 4
 			primaryPart.CFrame = primaryPart.CFrame:Lerp(CFrame.new(desiredPos),0.5)
-			camera.CFrame = CFrame.lookAt(camera.CFrame.Position, primaryPart.Position)
+			--camera.CFrame = CFrame.lookAt(camera.CFrame.Position, primaryPart.Position)
 		end)
 
 
